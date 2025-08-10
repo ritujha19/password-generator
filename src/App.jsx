a37af382216f48a853c3b58eff3405159b7a4c5f
ritu jha <ritu05491@gmail.com>
2025-08-10 22:44:04 +0530

    updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-
-import './App.css'
+import { useState, useCallback } from "react";
+import "./App.css";
 
 function App() {
-  
+  const [length, setLength] = useState(8);
+  const [numAllow, setNumAllow] = useState(false);
+  const [charAllow, setCharAllow] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numAllow) str += "0123456789";
+    if (charAllow) str += "!@#$^{}[]()&*_-~?/|`";
+
+    for (let i = 0; i < length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [numAllow, charAllow, length]);
 
   return (
     <>
+      <h1 className="text-4xl ">Password Generator</h1>
       
     </>
-  )
+  );
 }
 
-export default App
+export default App;
