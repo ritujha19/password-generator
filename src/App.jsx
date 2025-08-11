9f219ac1cd74db5318bd07077e63803685f521ae
ritu jha <ritu05491@gmail.com>
2025-08-11 20:59:49 +0530

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useCallback } from "react";
-import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -23,8 +22,22 @@ function App() {
 
   return (
     <>
-      <h1 className="text-4xl ">Password Generator</h1>
-      
+      <div className=" w-full h-screen p-10 text-center bg-gradient-to-br from-blue-950 to-purple-950  rounded-lg">
+        <h1 className="text-4xl px-4 py-4 text-amber-200">
+          Password Generator
+        </h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 items-center justify-center">
+          <input
+            type="text"
+            value={password}
+            className="w-xl h-12 text-black bg-amber-100 my-5 rounded-l-2xl p-3 text-2xl font-semibold"
+            placeholder="Password"
+            readOnly
+          />
+          <button className="h-12 bg-purple-200 text-black rounded-r-2xl">Copy</button>
+        </div>
+        
+      </div>
     </>
   );
 }
